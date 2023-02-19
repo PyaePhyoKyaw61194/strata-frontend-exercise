71a080b8dac46493f838ca1b054ae5c01f5a7e33
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2023-02-19 12:12:56 +0900

    modify(UI): adding back button in Error component

--- components/base/ErrorComp.tsx
+++ components/base/ErrorComp.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
+import LinkButton from "./LinkButton";
 
 const ErrorComp: FC<ErrorMessage> = ({ message }) => {
     return (
-        <div className="p-20 w-full flex flex-col items-center justify-center space-y-12 text-gray-900">
-            <div>
-                <p >An error has occurred:  {message}.</p>
-            </div>
+        <div className="w-full py-20 sm:flex sm:flex-col items-center justify-center space-y-12 text-gray-900 px-2">
+            <p >An error has occurred:  {message}.</p>
+            <LinkButton name="Back" url="/leaderboard" />
         </div>
     )
 }
--- pages/[...].tsx
+++ pages/[...].tsx
@@ -3,10 +3,7 @@ import { ErrorComp, LinkButton } from "../components/base"
 
 const CatchAll: FC = () => {
     return (
-        <div className="w-full py-20 sm:flex sm:flex-col items-center justify-center space-y-12 text-gray-900 px-2">
-            <ErrorComp message="Page Not Found" />
-            <LinkButton name="Back" url="/leaderboard" />
-        </div>
+        <ErrorComp message="Page Not Found" />
     )
 }
 
